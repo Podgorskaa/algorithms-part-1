eaf2d2190cd448c48f4228beabfadb251d279571
Podgorskaa <127777354+Podgorskaa@users.noreply.github.com>
2023-03-24 15:01:05 +0100

    Create WeightedQuickUnionUF.java

---
+++ WeightedQuickUnionUF.java
@@ -0,0 +1,42 @@
+public class WeightedQuickUnionUF {
+    private int[] id;
+    private int[] size;
+
+    public WeightedQuickUnionUF(int N) {
+        id = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int root(int i) {
+        while (i != id[i]){
+            i = id[i]; 
+        } 
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = root(p);
+        int rootQ = root(q);
+        if (rootP == rootQ) return;
+
+        // make smaller root point to larger one
+        if (size[rootP] < size[rootQ]) {
+            id[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        else {
+            id[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+    }
+}
+
+/* Depth of any node x is at most lg N */
